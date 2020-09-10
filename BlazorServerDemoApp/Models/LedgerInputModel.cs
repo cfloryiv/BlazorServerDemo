5d348d502705875eb243e1b6109dc5d96a0612d7
curtis flory <cfloryiv@rcn.com>
2020-09-10 11:41:29 -0400

    init commit

---
+++ BlazorServerDemoApp/Models/LedgerInputModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DataLibrary.Models;
+
+namespace BlazorServerDemoApp.Models
+{
+    public class LedgerInputModel
+    {
+        public int Year { get; set; }
+        public decimal Budget { get; set; }
+        public decimal Actual { get; set; }
+        public AccountModel Account { get; set; }
+        public decimal Variance { get; set; }
+
+    }
+}
---
+++ BlazorServerDemoApp/Startup.cs
@@ -0,0 +1,77 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using DataLibrary.Db;
+using DataLibrary.Data;
+
+namespace BlazorServerDemoApp
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddRazorPages();
+            services.AddServerSideBlazor();
+            services.AddSingleton(new ConnectionStringData
+            {
+                SqlConnectionName = "Default"
+            });
+            services.AddSingleton<IDataAccess, SqlDb>();
+            services.AddSingleton<IFoodData, FoodData>();
+            services.AddSingleton<IOrderData, OrderData>();
+            services.AddSingleton<IBookData, BookData>();
+            services.AddSingleton<ICourseData, CourseData>();
+            services.AddSingleton<IDoctorData, DoctorData>();
+            services.AddSingleton<IMedicalVisitData, MedicalVisitData>();
+            services.AddSingleton<IMedicineData, MedicineData>();
+            services.AddSingleton<IRefillData, RefillData>();
+            services.AddSingleton<ITrackingData, TrackingData>();
+            services.AddSingleton<IAccountData, AccountData>();
+            services.AddSingleton<ILedgerData, LedgerData>();
+            services.AddSingleton<ISugarData, SugarData>();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapBlazorHub();
+                endpoints.MapFallbackToPage("/_Host");
+            });
+        }
+    }
+}
